f768bce8efc8f5b48930616105859fc0542c2fcc
g12925kr <g12925kr@s30425.tsuda.ac.jp>
2014-05-09 11:49:50 +0900

    ３０分経過

---
+++ IntToEng.java/src/IntToEng.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IntToEng {
+	public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int input = sc.nextInt();
+			
+			System.out.println(translateEng(input));
+	}
+	
+	//数値を英訳するメソッド
+		static String translateEng(int n) {
+			//LinearSearch table = new LinearSearch();
+			return "";
+		}
+}
